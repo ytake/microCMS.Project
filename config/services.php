806a065d3d936337ff592863b0c0ec4c3d041b9b
yuuki takezawa <yuuki.takezawa@comnect.jp.net>
2015-05-11 19:35:17 +0900

    oops

--- config/services.php
+++ config/services.php
@@ -3,9 +3,9 @@
 return [
 
     'github' => [
-        'client_id' => 'ae78689f81b145fc0681',
-        'client_secret' => 'c8f4990c3a080a714591d125c780ea22385b6f45',
-        'redirect' => 'http://cms.comnect.jp.net/callback',
+        'client_id' => 'your-github-app-id',
+        'client_secret' => 'your-github-app-secret',
+        'redirect' => 'http://your-callback-url',
     ],
 
     'facebook' => [
